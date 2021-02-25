1a4dfdc4f2bd9cf367b5a0a21903da0dc540ef86
Paritosh Wankhade <59663483+wpn10@users.noreply.github.com>
2021-02-26 04:27:57 +0530

    Create Visual.m
    The Data-Sets were formed (gpm.xlsx, TERLS.xlsx) using the Overpass and the same Visualised with Matlab

---
+++ Cross-Validation/Visual.m
@@ -0,0 +1,30 @@
+data = xlsread("gpm.xlsx");
+x = data(:,2);
+y = data(:,3);
+z = data(:,1);
+
+
+%Uncomment for the Continous-2D-Countour-plot
+  
+% xi=linspace(min(x),max(x),30);
+% yi=linspace(min(y),max(y),30);
+% [XI, YI]=meshgrid(xi,yi);
+% ZI = griddata(x,y,z,XI,YI);
+% colormap('jet');
+% % for i=1:30
+% %     for j=1:30
+% %         if(isnan(ZI(i,j)))
+% %             ZI(i,j)=0;
+% %         end
+% %     end
+% % end
+% 
+% contourf(XI,YI,ZI);
+colormap('jet');
+scatter(x,y,[],z,'fill');
+title('Reflectivity (DBZ) on 2019-06-12 at 23:55:31');
+xlabel('Latitudes degress North');
+ylabel('Longitudes degrees East');
+colorbar;
+h = colorbar;
+ylabel(h, 'Reflectivity(DBZ)');
